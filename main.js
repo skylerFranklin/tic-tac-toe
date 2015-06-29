a519e3735628c14a36309f79b684ed9b408784d6
unknown <skylerf2003@gmail.com>
2015-06-29 10:57:00 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// what do we need in tic-tac-toe 
+// board |1|2|3|
+         |4|5|6| 
+         |7|8|9| 
+             
+var gameBoard = [ ["1","2","3"], ["4","5","6"], ["7","8","9"]]; 
+// first player to get three in a row wins // x's and o's 
+var player1 = "x"; 
+var player2 = "o"; 
+// assign each player a symbol 
+// 2 players
+player1 = true; 
+player2 = false; 
+// alternating turns 
+function alternatingTurns() {
+
+} 
+// scoreboard 
+//add one to the winners score
